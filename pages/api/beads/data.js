0f7988dc37eb3aa1a2be8651168ec790b37cc517
amankaushik0874 <aman.kaushik@sof.to>
2025-06-08 22:41:12 +0530

    add data

--- pages/api/beads/data.js
+++ pages/api/beads/data.js
@@ -1,19 +1,19 @@
 export const dummyBeads = {
-  "442NUGOO": {
+  "B9J6HHVD": {
     type: "Necklace",
     weight: 12.5,
     dimensions: "10x5x3",
     materials: "Gold",
     origin: "Italy"
   },
-  "42NUGOO4": {
+  "9J6HHVDI": {
     type: "Bracelet",
     weight: 8.2,
     dimensions: "8x4x2",
     materials: "Gold",
     origin: "France"
   },
-  "2NUGOO41": {
+  "J6HHVDIJ": {
     type: "Ring",
     weight: 3.1,
     dimensions: "2x2x1",

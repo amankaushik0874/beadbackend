8204db94f27dd3ffb9b7c00dd1a00fb1ced72fce
amankaushik0874 <aman.kaushik@sof.to>
2025-06-09 20:08:13 +0530

    update backend

--- pages/api/beads/data.js
+++ pages/api/beads/data.js
@@ -1,19 +1,19 @@
 export const dummyBeads = {
-  "B9J6HHVD": {
+  "VCZAF059": {
     type: "Necklace",
     weight: 12.5,
     dimensions: "10x5x3",
     materials: "Gold",
     origin: "Italy"
   },
-  "9J6HHVDI": {
+  "CZAF059P": {
     type: "Bracelet",
     weight: 8.2,
     dimensions: "8x4x2",
     materials: "Gold",
     origin: "France"
   },
-  "J6HHVDIJ": {
+  "ZAF059PQ": {
     type: "Ring",
     weight: 3.1,
     dimensions: "2x2x1",
